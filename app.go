fc1ca2f1e582fa222dcb266b713c63fb2338101e
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-09 17:59:40 +0400

    refactor(app): move all main logic into app.go

---
+++ app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type pokemon struct {
+	Id int `json:"id"`
+}
+
+func GetPokemonInfo(pokemonName string) (pokemon, error) {
+	res, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s/", pokemonName))
+	if err != nil {
+		return pokemon{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return pokemon{}, fmt.Errorf("failed to retrieve Pokémon info: %s", res.Status)
+	}
+
+	defer res.Body.Close()
+
+	var result pokemon
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&result); err != nil {
+		return pokemon{}, err
+	}
+
+	return result, nil
+}
