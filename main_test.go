7c4cd1be0185e1e0e561d6453a309d60520be1bb
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-09 16:00:59 +0400

    feat(test): implement tests for getting Id

---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetPokemonId(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		want          int
+		wantErr       error
+		errorExpected bool
+	}{
+		{name: "Normal case", input: "pichu", want: 172, wantErr: nil, errorExpected: false},
+		{name: "Non-existent Pokémon", input: "invalidpokemon", want: 0, wantErr: fmt.Errorf("failed to retrieve Pokémon info: 404 Not Found"), errorExpected: true},
+		{name: "Empty Pokémon name", input: "", want: 0, wantErr: fmt.Errorf("failed to retrieve Pokémon info: 404 Not Found"), errorExpected: true},
+		{name: "Pokémon name with multiple words", input: "mr-mime", want: 122, wantErr: nil, errorExpected: false},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetPokemonInfo(tt.input)
+			if !tt.errorExpected && err != tt.wantErr {
+				t.Errorf("input: %v - got: %v, %v - want: %v, %v", tt.input, got, err, tt.want, tt.wantErr)
+			} else if err == tt.wantErr && tt.wantErr != nil {
+				t.SkipNow()
+			}
+
+			if got.Id != tt.want {
+				t.Errorf("input: %v - got: %v, %v - want: %v, %v", tt.input, got, err, tt.want, tt.wantErr)
+			}
+		})
+	}
+}
