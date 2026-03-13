d591a299c78816d5838e3b28a6f439a88ee104aa
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-13 17:03:27 +0400

    feat(app): add getting types

--- app.go
+++ app.go
@@ -22,6 +22,13 @@ type pokemon struct {
 			Name string `json:"name"`
 		}
 	} `json:"abilities"`
+
+	Stats []struct {
+		Stat     int `json:"base_stat"`
+		StatInfo struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
 }
 
 // GetPokemonInfo sends a GET request to the Pokémon API to retrieve information for a given Pokémon.
@@ -61,6 +68,38 @@ func GetPokemonAbilities(p pokemon) []string {
 	var result []string
 	for _, t := range p.Abilities {
 		result = append(result, t.Ability.Name)
+	}
+	return result
+}
+
+type BaseStats struct {
+	HP      int
+	Attack  int
+	Defense int
+	SpAtk   int
+	SpDef   int
+	Speed   int
+}
+
+// Extracts and returns a BaseStats struct from the given struct
+func GetPokemonBaseStats(p pokemon) BaseStats {
+	var result BaseStats
+
+	for _, stat := range p.Stats {
+		switch stat.StatInfo.Name {
+		case "hp":
+			result.HP = stat.Stat
+		case "attack":
+			result.Attack = stat.Stat
+		case "defense":
+			result.Defense = stat.Stat
+		case "special-attack":
+			result.SpAtk = stat.Stat
+		case "special-defense":
+			result.SpDef = stat.Stat
+		case "speed":
+			result.Speed = stat.Stat
+		}
 	}
 	return result
 }
