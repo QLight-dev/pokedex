82c1a3ca70917d82279b5142a5c31e2a28afe057
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-14 18:12:22 +0400

    feat(utils): add converting dm to m

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,15 @@
 package utils
 
-const HectogramsToKgConversionRate = float32(10)
+const hectogramsToKgConversionRate = float32(10)
 
-// Converts hectograms to kilograms using a conversion rate of 10.
-// The conversion rate represents how many kilograms correspond to one hectogram.
+// Convert hectograms to kilograms.
 func HectogramsToKg(initial float32) float32 {
-	return initial / HectogramsToKgConversionRate
+	return initial / hectogramsToKgConversionRate
+}
+
+const decimetersToMetersConversionRate = float32(10)
+
+// Convert decimeters to meters.
+func DecimetersToMeters(initial float32) float32 {
+	return initial / decimetersToMetersConversionRate
 }
